0e629a796c47d8b055e060211ff5ff4818226beb
Joanna Czubasiewicz <joanna.czubasiewicz@gmail.com>
2020-01-08 09:42:32 +0100

    Add heroku kttp adress to readme file

---
+++ README.md
@@ -0,0 +1,1 @@
+https://pizzeria-module8.herokuapp.com/
--- src/js/components/HourPicker.js
+++ src/js/components/HourPicker.js
@@ -9,186 +9,186 @@ import Booking from '/js/components/Booking.js';
 
 
 class HourPicker extends BaseWidget {
-    constructor(wrapper) {
-        super(wrapper, settings.hours.open);
-        const thisWidget = this;
+  constructor(wrapper) {
+    super(wrapper, settings.hours.open);
+    const thisWidget = this;
 
-        thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.input);
-        thisWidget.dom.output = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.output);
-        thisWidget.initPlugin();
-        thisWidget.value = thisWidget.dom.input.value;
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.input);
+    thisWidget.dom.output = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.output);
+    thisWidget.initPlugin();
+    thisWidget.value = thisWidget.dom.input.value;
 
 
 
 
 
-    }
+  }
 
-    tableCheck() {
-        const thisWidget = this;
-        // odczytac z JSON zareserwowane stoliki dla daty i godziny
+  tableCheck() {
+    const thisWidget = this;
+    // odczytac z JSON zareserwowane stoliki dla daty i godziny
 
-        //licznik ustawiamy na 0
-        let licznik = 0;
+    //licznik ustawiamy na 0
+    let licznik = 0;
 
-        const url = settings.db.url + '/' + settings.db.event;
-        const url2 = settings.db.url + '/' + settings.db.booking;
+    const url = settings.db.url + '/' + settings.db.event;
+    const url2 = settings.db.url + '/' + settings.db.booking;
 
 
-        const bookings = {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(),
-        };
+    const bookings = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(),
+    };
 
 
 
-        fetch(url, bookings)
-            .then(function(response) {
-                return response.json();
-            }).then(function(parsedResponse) {
+    fetch(url, bookings)
+      .then(function(response) {
+        return response.json();
+      }).then(function(parsedResponse) {
 
 
-                const events = parsedResponse;
+        const events = parsedResponse;
 
-                //szukanie po elemntach
-                for (let ocupied of events) {
-                    //console.log('ocupied', ocupied);
-                    let ocupiedHour = utils.hourToNumber(ocupied.hour);
+        //szukanie po elemntach
+        for (let ocupied of events) {
+          //console.log('ocupied', ocupied);
+          let ocupiedHour = utils.hourToNumber(ocupied.hour);
 
 
-                    let ocupiedDuration = ocupied.duration;
+          let ocupiedDuration = ocupied.duration;
 
 
-                    let ocupiedDupa = ocupiedHour + ocupiedDuration;
-                    //console.log('suma ocupiedHour', ocupiedHour);
-                    //console.log('suma ocupied + duration', ocupiedDupa);
-                    //console.log('IF (' + ocupiedDupa + ' > ' + utils.hourToNumber(thisWidget.dom.output.value) + '>' + ocupiedHour + ' )');
+          let ocupiedDupa = ocupiedHour + ocupiedDuration;
+          //console.log('suma ocupiedHour', ocupiedHour);
+          //console.log('suma ocupied + duration', ocupiedDupa);
+          //console.log('IF (' + ocupiedDupa + ' > ' + utils.hourToNumber(thisWidget.dom.output.value) + '>' + ocupiedHour + ' )');
 
-                    if ((ocupiedDupa > utils.hourToNumber(thisWidget.dom.output.value)) && (utils.hourToNumber(thisWidget.dom.output.value) > ocupiedHour)) {
-                        licznik++;
-                    }
-                    console.log('licznik', licznik);
-                }
-                //console.log('events', events);
+          if ((ocupiedDupa > utils.hourToNumber(thisWidget.dom.output.value)) && (utils.hourToNumber(thisWidget.dom.output.value) > ocupiedHour)) {
+            licznik++;
+          }
+          console.log('licznik', licznik);
+        }
+        //console.log('events', events);
 
-            });
+      });
 
-        fetch(url2, bookings)
-            .then(function(response) {
-                return response.json();
-            }).then(function(parsedResponse) {
-                console.log('parsedResponse booking:', parsedResponse);
+    fetch(url2, bookings)
+      .then(function(response) {
+        return response.json();
+      }).then(function(parsedResponse) {
+        console.log('parsedResponse booking:', parsedResponse);
 
-                const booked = parsedResponse;
+        const booked = parsedResponse;
 
-                // ustalenie gdzie ma być zmieniany kolor
-                /* eslint-disable no-unused-vars */
-                //let rangeSliderColour = document.querySelector('.rangeSlider_fill');
+        // ustalenie gdzie ma być zmieniany kolor
+        /* eslint-disable no-unused-vars */
+        //let rangeSliderColour = document.querySelector('.rangeSlider_fill');
 
 
 
-                //szuanie po elementach
-                for (let book of booked) {
+        //szuanie po elementach
+        for (let book of booked) {
 
-                    let bookHour = utils.hourToNumber(book.hour);
-                    //console.log('bookHour', bookHour);
+          let bookHour = utils.hourToNumber(book.hour);
+          //console.log('bookHour', bookHour);
 
-                    let bookDuration = book.duration;
-                    //console.log('bookDuration', bookDuration);
+          let bookDuration = book.duration;
+          //console.log('bookDuration', bookDuration);
 
-                    let bookDupa = bookHour + bookDuration;
-                    //console.log('suma booking i duration', bookDupa);
+          let bookDupa = bookHour + bookDuration;
+          //console.log('suma booking i duration', bookDupa);
 
 
-                    if ((bookDupa > utils.hourToNumber(thisWidget.dom.output.value)) && (utils.hourToNumber(thisWidget.dom.output.value) > bookHour)) {
-                        licznik++;
+          if ((bookDupa > utils.hourToNumber(thisWidget.dom.output.value)) && (utils.hourToNumber(thisWidget.dom.output.value) > bookHour)) {
+            licznik++;
 
 
 
-                    }
-                    thisWidget.hour = thisWidget.dom.output.value;
-                    //console.log('godzina Booking w hourPicker', thisWidget.hour);
+          }
+          thisWidget.hour = thisWidget.dom.output.value;
+          //console.log('godzina Booking w hourPicker', thisWidget.hour);
 
-                    console.log('licznik 2for', licznik);
-                }
-                console.log('liczniki poza forX', licznik);
+          console.log('licznik 2for', licznik);
+        }
+        console.log('liczniki poza forX', licznik);
 
-                let rangeSliderColour = document.getElementsByClassName('rangeSlider__fill')[0];
+        let rangeSliderColour = document.getElementsByClassName('rangeSlider__fill')[0];
 
 
-                if (licznik == 0) {
-                    console.log('dupa w if1');
-                    rangeSliderColour.style.background = 'green';
+        if (licznik == 0) {
+          console.log('dupa w if1');
+          rangeSliderColour.style.background = 'green';
 
-                }
-                if (licznik == 1) {
-                    console.log('dupa w if2');
-                    rangeSliderColour.style.background = 'yellow';
-                }
-                if (licznik == 2) {
-                    console.log('dupa w if3');
-                    rangeSliderColour.style.background = 'red';
-                }
-                if (licznik == 3) {
-                    rangeSliderColour.style.background = 'black';
-                }
+        }
+        if (licznik == 1) {
+          console.log('dupa w if2');
+          rangeSliderColour.style.background = 'yellow';
+        }
+        if (licznik == 2) {
+          console.log('dupa w if3');
+          rangeSliderColour.style.background = 'red';
+        }
+        if (licznik == 3) {
+          rangeSliderColour.style.background = 'black';
+        }
 
-            });
+      });
 
 
 
 
-    }
+  }
 
-    initPlugin() {
-        const thisWidget = this;
+  initPlugin() {
+    const thisWidget = this;
 
 
-        console.log('dom', thisWidget.dom.input);
+    console.log('dom', thisWidget.dom.input);
 
-        const slider = document.getElementById('slider1');
-        rangeSlider.create(thisWidget.dom.input, { polyfill: true });
-        //https://github.com/Stryzhevskyi/rangeSlider/blob/master/README.md
+    const slider = document.getElementById('slider1');
+    rangeSlider.create(thisWidget.dom.input, { polyfill: true });
+    //https://github.com/Stryzhevskyi/rangeSlider/blob/master/README.md
 
 
 
 
 
-        thisWidget.dom.input.addEventListener('input', function() {
-            thisWidget.value = thisWidget.dom.input.value;
+    thisWidget.dom.input.addEventListener('input', function() {
+      thisWidget.value = thisWidget.dom.input.value;
 
 
-            //wywolac tables check
-            thisWidget.tableCheck();
+      //wywolac tables check
+      thisWidget.tableCheck();
 
-        });
+    });
 
-    }
+  }
 
-    parseValue(value) {
+  parseValue(value) {
 
-        const numberHour = utils.numberToHour(value);
+    const numberHour = utils.numberToHour(value);
 
 
-        return (numberHour);
-    }
+    return (numberHour);
+  }
 
-    isValid() { // eslint-disable-line no-unused-vars
-        return (true);
-    }
+  isValid() { // eslint-disable-line no-unused-vars
+    return (true);
+  }
 
-    renderValue() {
-        const thisWidget = this;
-        thisWidget.dom.output.value = thisWidget.value;
-        thisWidget.dom.output.innerHTML = thisWidget.value;
+  renderValue() {
+    const thisWidget = this;
+    thisWidget.dom.output.value = thisWidget.value;
+    thisWidget.dom.output.innerHTML = thisWidget.value;
 
 
 
 
-    }
+  }
 
 }
 export default HourPicker;
